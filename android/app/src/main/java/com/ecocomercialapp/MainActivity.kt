6f4657f4cdd4dfa7d49311733600e01a1479d906
Daniel <danielmanjarres6@gmail.com>
2025-08-01 23:52:52 -0500

    ya funciona

--- android/app/src/main/java/com/ecocomercialapp/MainActivity.kt
+++ android/app/src/main/java/com/ecocomercialapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ecocomercialapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +8,15 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  // Nombre del componente principal registrado desde JavaScript
   override fun getMainComponentName(): String = "EcoComercialApp"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
+  // Delegate que permite usar la nueva arquitectura (si está habilitada)
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // NECESARIO para que el módulo react-native-gesture-handler funcione correctamente
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
--- android/app/src/main/java/com/ecocomercialapp/MainApplication.kt
+++ android/app/src/main/java/com/ecocomercialapp/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -17,14 +18,11 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(RNGestureHandlerPackage()) // Aseguramos que se registre correctamente
             }
 
         override fun getJSMainModuleName(): String = "index"
-
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
@@ -36,7 +34,6 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
   }
